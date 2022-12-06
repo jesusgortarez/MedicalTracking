d87573c1c34bc0c39b530e0f0672753d2f624d89
jesusgortarez <jesus.gortarez@uabc.edu.mx>
2022-12-05 21:17:12 -0800

    Navegador Informacion

---
+++ app/src/main/java/com/jesus/medicaltracking/ui/informacion/InformacionFragment.java
@@ -0,0 +1,35 @@
+package com.jesus.medicaltracking.ui.informacion;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.jesus.medicaltracking.databinding.FragmentInformacionBinding;
+
+public class InformacionFragment extends Fragment {
+
+    private FragmentInformacionBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        InformacionViewModel informacionViewModel =
+                new ViewModelProvider(this).get(InformacionViewModel.class);
+
+        binding = FragmentInformacionBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
--- app/src/main/java/com/jesus/medicaltracking/ui/informacion/InformacionViewModel.java
+++ app/src/main/java/com/jesus/medicaltracking/ui/informacion/InformacionViewModel.java
@@ -1,16 +1,16 @@
-package com.jesus.medicaltracking.ui.notifications;
+package com.jesus.medicaltracking.ui.informacion;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class InformacionViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public InformacionViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is information fragment");
     }
 
     public LiveData<String> getText() {
