59bacd88f8e242abde676cbc88c44171f30d458a
jesusgortarez <jesus.gortarez@uabc.edu.mx>
2022-12-07 15:59:12 -0800

    Acerca de con listview

---
+++ app/src/main/java/com/jesus/medicaltracking/ListAcercaDeAdapter.java
@@ -0,0 +1,40 @@
+package com.jesus.medicaltracking;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ListAcercaDeAdapter extends ArrayAdapter<ListAcercaDe> {
+
+    ArrayList<ListAcercaDe> listAcercaDe;
+
+    public ListAcercaDeAdapter(@NonNull Context context, int resource, @NonNull ArrayList<ListAcercaDe> listAcercaDe) {
+        super(context, resource, listAcercaDe);
+        this.listAcercaDe = listAcercaDe;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+
+        convertView = layoutInflater.inflate(R.layout.item_acerca_de,parent,false);
+
+        TextView listAcercaDeNameTextView = convertView.findViewById(R.id.listAcercaDeNameTextView);
+        TextView listAcercaDeDataTextView = convertView.findViewById(R.id.listAcercaDeDataTextView);
+
+        listAcercaDeNameTextView.setText(listAcercaDe.get(position).toString());
+        listAcercaDeDataTextView.setText(listAcercaDe.get(position).toString());
+        return convertView;
+    }
+}
--- app/src/main/java/com/jesus/medicaltracking/ui/acercaDe/AcercaDeFragment.java
+++ app/src/main/java/com/jesus/medicaltracking/ui/acercaDe/AcercaDeFragment.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.jesus.medicaltracking.ListAcercaDe;
+import com.jesus.medicaltracking.ListAcercaDeAdapter;
+import com.jesus.medicaltracking.R;
 import com.jesus.medicaltracking.databinding.FragmentAcercaDeBinding;
 
+import java.util.ArrayList;
+
 public class AcercaDeFragment extends Fragment {
+
+    ArrayList<ListAcercaDe> listAcercaDeArrayList;
+    ListView listAcercaDeListView;
 
     private FragmentAcercaDeBinding binding;
 
@@ -22,6 +31,19 @@ public class AcercaDeFragment extends Fragment {
 
         binding = FragmentAcercaDeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        listAcercaDeListView = root.findViewById(R.id.listAcercaDeListView);
+        listAcercaDeArrayList = new ArrayList<ListAcercaDe>();
+
+        listAcercaDeArrayList.add(new ListAcercaDe(1,"Razon de la app", "bla bla"));
+        listAcercaDeArrayList.add(new ListAcercaDe(2,"Diseñadores", "bla bla"));
+        listAcercaDeArrayList.add(new ListAcercaDe(3,"Version", "bla bla"));
+        listAcercaDeArrayList.add(new ListAcercaDe(4,"ultima fecha de vercion", "bla bla"));
+        listAcercaDeArrayList.add(new ListAcercaDe(5,"no se que mas", "bla bla"));
+        listAcercaDeArrayList.add(new ListAcercaDe(5,"GitHub", "bla bla"));
+
+        ListAcercaDeAdapter listAcercaDeArrayAdapter = new ListAcercaDeAdapter(getActivity().getApplicationContext(),R.layout.item_acerca_de,listAcercaDeArrayList);
+
+        listAcercaDeListView.setAdapter(listAcercaDeArrayAdapter);
 
 
         return root;
