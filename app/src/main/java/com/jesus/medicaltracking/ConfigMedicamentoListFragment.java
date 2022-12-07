d68fdaa16aee196bf7361211e17f76119c960c98
jesusgortarez <jesus.gortarez@uabc.edu.mx>
2022-12-07 00:23:50 -0800

    Config y sus fragment para que se sepa que hacer

---
+++ app/src/main/java/com/jesus/medicaltracking/ConfigMedicamentoListFragment.java
@@ -0,0 +1,18 @@
+package com.jesus.medicaltracking;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConfigMedicamentoListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_config_medicamento_list, container, false);
+    }
+}
