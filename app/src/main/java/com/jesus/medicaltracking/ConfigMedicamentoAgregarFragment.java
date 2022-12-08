94fa9afb9e6d7d4b801fb56ab6e2951da2a7b8db
diego <diego.ivan.lopez.nunez@uabc.edu.mx>
2022-12-07 21:51:23 -0800

    ConfigMedicamentoListFragment Actualizar al agregar

--- app/src/main/java/com/jesus/medicaltracking/ConfigMedicamentoAgregarFragment.java
+++ app/src/main/java/com/jesus/medicaltracking/ConfigMedicamentoAgregarFragment.java
@@ -3,6 +3,7 @@ package com.jesus.medicaltracking;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,8 @@ import io.realm.Realm;
 public class ConfigMedicamentoAgregarFragment extends Fragment {
     private Realm con;
 
+    FragmentTransaction transaction;
+    Fragment fragmentlista;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,7 +42,12 @@ public class ConfigMedicamentoAgregarFragment extends Fragment {
                 con.copyToRealmOrUpdate(medicamento);
                 con.commitTransaction();
 
+                fragmentlista = new ConfigMedicamentoListFragment();
 
+
+                getActivity().getSupportFragmentManager().beginTransaction().add(R.id.listaMedicamentosFragment,fragmentlista).commit();
+                transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.listaMedicamentosFragment,fragmentlista).commit();
             }
         });
         return view;
