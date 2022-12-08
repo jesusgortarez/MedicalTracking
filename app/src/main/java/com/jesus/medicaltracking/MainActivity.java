f8bcca5835e5963b6bd1c6e50814bf85b89f5367
jesusgortarez <jesus.gortarez@uabc.edu.mx>
2022-12-08 12:22:28 -0800

    3 a 2 botones

--- app/src/main/java/com/jesus/medicaltracking/MainActivity.java
+++ app/src/main/java/com/jesus/medicaltracking/MainActivity.java
@@ -64,8 +64,14 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_inicio, R.id.navigation_acerca_de)
+                .build();
+        /*
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_inicio, R.id.navigation_acerca_de, R.id.navigation_informacion)
                 .build();
+
+         */
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
