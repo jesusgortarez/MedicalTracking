371944c462e7bf406e4f6a186a778ac75c7d00bd
jesusgortarez <jesus.gortarez@uabc.edu.mx>
2022-12-08 15:22:09 -0800

    Entrega mejorada

--- app/src/main/java/com/jesus/medicaltracking/ui/acercaDe/AcercaDeFragment.java
+++ app/src/main/java/com/jesus/medicaltracking/ui/acercaDe/AcercaDeFragment.java
@@ -1,10 +1,13 @@
 package com.jesus.medicaltracking.ui.acercaDe;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -44,18 +47,28 @@ public class AcercaDeFragment extends Fragment {
         listAcercaDeListView = root.findViewById(R.id.listAcercaDeListView);
         listAcercaDeArrayList = new ArrayList<ListAcercaDe>();
 
-        listAcercaDeArrayList.add(new ListAcercaDe(1, tag_about, "blsdfsdfsfd"));
-        listAcercaDeArrayList.add(new ListAcercaDe(2, tag_design, "bla bla"));
-        listAcercaDeArrayList.add(new ListAcercaDe(3, tag_ver, "bla bla"));
-        listAcercaDeArrayList.add(new ListAcercaDe(4, tag_lver, "bla bla"));
-        listAcercaDeArrayList.add(new ListAcercaDe(5,"no se que mas", "bla bla"));
+        listAcercaDeArrayList.add(new ListAcercaDe(1, tag_about, "Una aplicación móvil con el propósito de llevar un seguimiento y control sobre los medicamentos que son utilizados por el usuario en su vida diaria además de poder llevar un registro sobre los efectos secundarios producidos por consumir medicamentos en específico."));
+        listAcercaDeArrayList.add(new ListAcercaDe(2, tag_design, "Jesus Javier Gortarez Pelayo \nDiego Ivan Lopez Nuñes"));
+        listAcercaDeArrayList.add(new ListAcercaDe(3, tag_ver, "Alpha 0.5.0"));
+        listAcercaDeArrayList.add(new ListAcercaDe(4, tag_lver, "08/12/2022"));
         listAcercaDeArrayList.add(new ListAcercaDe(6,"GitHub", "https://github.com/jesusgortarez/medicaltracking"));
 
         ListAcercaDeAdapter listAcercaDeArrayAdapter = new ListAcercaDeAdapter(getActivity().getApplicationContext(),R.layout.item_acerca_de,listAcercaDeArrayList);
 
         listAcercaDeListView.setAdapter(listAcercaDeArrayAdapter);
 
+        //detectar si es precionado algo del layout
+        listAcercaDeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int id = ((ListAcercaDe)adapterView.getItemAtPosition(i)).getId();
+                if (id==6){
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(((ListAcercaDe)adapterView.getItemAtPosition(i)).getData()));
+                    startActivity(browserIntent);
+                }
 
+            }
+        });
         return root;
     }
 
